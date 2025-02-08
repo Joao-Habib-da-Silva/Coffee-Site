c9ad6effb1acbe0fe490358e0ddc8997b866e4d5
Joao Habib da Silva <contajh544@gmail.com>
2025-02-07 21:22:59 -0300

    Start a little project to desenvolve my design abilities

---
+++ script.js
@@ -0,0 +1,30 @@
+var navbar = window.document.getElementById("navbar")
+function Click() {
+    navbar.classList.toggle("open")
+}
+
+
+var html = window.document.documentElement
+    
+var  ball= window.document.getElementById("ball")
+function Theme() {
+    ball.classList.toggle("darktheme")
+    html.classList.toggle("darktheme")
+    if(html.classList.contains("darktheme")) {
+        localStorage.setItem("theme", "dark")
+    }
+    else {
+        localStorage.setItem("theme", "light")
+    }
+}
+window.onload = function() {
+    var theme = window.localStorage.getItem("theme")
+    if (theme === "dark") {
+        ball.classList.add("darktheme")
+        html.classList.add("darktheme")
+    }
+    else if (theme === "light") {
+        ball.classList.remove("darktheme")
+        html.classList.remove("darktheme")
+    }
+}
